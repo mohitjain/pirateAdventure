cee05de9020549caf695d97e717afcddb1a773b1
Mohit Jain <jain.mohit27@gmail.com>
2014-08-30 04:45:47 +0530

    Initial Commit

---
+++ Pirate Adventure/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Pirate Adventure
+//
+//  Created by Mohit Jain on 30/08/14.
+//  Copyright (c) 2014 CodeBeerStartups. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Adventure/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Pirate Adventure
+//
+//  Created by Mohit Jain on 30/08/14.
+//  Copyright (c) 2014 CodeBeerStartups. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
